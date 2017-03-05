ea64ebbd0bba13fd2b25a1d3b3d8238eb0ce7dca
sceptross <up201304205@fe.up.pt>
2017-03-05 16:16:55 +0000

    Add Platforms

---
+++ Assets/Scripts/GeneratePlatformsScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class GeneratePlatformsScript : MonoBehaviour {
+    public int posY = (int)GameObject.Find("background").GetComponent<ScrollingScript>().transform.position.y;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var curPosY = (int)GameObject.Find("background").GetComponent<ScrollingScript>().transform.position.y;
+
+        if (posY - curPosY != 0)
+        {
+            var dist = (transform.position - Camera.main.transform.position).z;
+            posY = curPosY;
+            Vector3 position = new Vector3(UnityEngine.Random.value * Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).x,
+                Camera.main.ViewportToWorldPoint(new Vector3(1, 0, dist)).y,
+                dist);
+            GameObject obj = Instantiate(Resources.Load("Prefabs/Platform")) as GameObject;
+            obj.transform.position = position;
+
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformMovementScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovementScript : MonoBehaviour {
+
+    public Vector2 speed = new Vector2(5, 0);
+    public Vector2 direction = new Vector2(-1, -1);
+    public bool isMovingPlatform = false;
+
+    private Rigidbody2D rigidbodyComponent;
+    private Vector2 movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed.y = GameObject.Find("background").GetComponent<ScrollingScript>().speed.y;
+
+        movement = new Vector2(
+          speed.x * direction.x,
+          speed.y * direction.y);
+
+        if (isMovingPlatform) {
+            var dist = (transform.position - Camera.main.transform.position).z;
+            var rightBorder = Camera.main.ViewportToWorldPoint(
+                new Vector3(1, 0, dist)
+               ).x;
+            var leftBorder = Camera.main.ViewportToWorldPoint(
+                new Vector3(0, 0, dist)
+               ).x;
+
+            if (transform.position.x <= leftBorder)
+                direction = new Vector2(1, -1);
+            else if (transform.position.x >= rightBorder)
+                direction = new Vector2(-1, -1);
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+        if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
+
+        // Apply movement to the rigidbody
+        rigidbodyComponent.velocity = movement;
+    }
+}
