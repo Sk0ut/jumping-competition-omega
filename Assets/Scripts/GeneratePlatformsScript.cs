abe32ffde8537a9bebefeb3c398c7789686cc909
Sk0ut <fcouto1995@gmail.com>
2017-03-05 18:37:54 +0000

    Add missing files

--- Assets/Scripts/GeneratePlatformsScript.cs
+++ Assets/Scripts/GeneratePlatformsScript.cs
@@ -3,27 +3,56 @@ using UnityEngine;
 
 public class GeneratePlatformsScript : MonoBehaviour {
 	public Camera camera;
-	private float posY;
 	public GameObject prefab;
 	public float gapY = 1f;
+	public int poolSize = 5;
+
+	private float posY;
+	private GameObject[] objectPool;
 
 	// Use this for initialization
 	void Start () {
 		posY = camera.transform.position.y + camera.orthographicSize;
+		objectPool = new GameObject[poolSize];
+		for (int i = 0; i < objectPool.Length; ++i) {
+			objectPool [i] = Instantiate (prefab);
+			objectPool [i].SetActive (false);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float curPosY = camera.transform.position.y + camera.orthographicSize;
+		clearOffScreen ();
 
         if (curPosY - posY > gapY)
         {
 			Debug.Log ("Time to generate");
-            posY = curPosY;
             Vector3 position = new Vector3(UnityEngine.Random.value * 12f - 6f,
                	curPosY, 0);
-            GameObject obj = Instantiate(prefab);
-            obj.transform.position = position;
+			if (generatePlatform (position)) {
+				Debug.Log ("New object");
+				posY = curPosY;
+			}
         }
     }
+
+	bool generatePlatform(Vector3 position){
+		for (int i = 0; i < objectPool.Length; ++i) {
+			if (!objectPool [i].activeSelf) {
+				objectPool [i].transform.position = position;
+				objectPool [i].SetActive (true);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	void clearOffScreen() {
+		for (int i = 0; i < objectPool.Length; ++i) {
+			if (objectPool [i].activeSelf && objectPool[i].transform.position.y < camera.transform.position.y - camera.orthographicSize) {
+				objectPool [i].SetActive (false);
+			}
+		}
+	}
 }
---
+++ Assets/Scripts/TestScripts/CameraScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroll : MonoBehaviour {
+
+	public float stepY = 0.01f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		transform.position = new Vector3(transform.position.x, transform.position.y + stepY, transform.position.z);
+	}
+}
