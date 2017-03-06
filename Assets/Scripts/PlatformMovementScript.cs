81d263eedb0d5165782aa6606e1d95c46054d856
quartz55 <john.razor97@gmail.com>
2017-03-06 19:09:33 +0000

    Added moving platforms to game scene

--- Assets/Scripts/PlatformMovementScript.cs
+++ Assets/Scripts/PlatformMovementScript.cs
@@ -6,38 +6,32 @@ public class PlatformMovementScript : MonoBehaviour {
 
     public Vector2 speed = new Vector2(5, 0);
     public Vector2 direction = new Vector2(-1, -1);
-    public bool isMovingPlatform = false;
 
     private Rigidbody2D rigidbodyComponent;
-    private Vector2 movement;
+    public Vector2 movement;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
         movement = new Vector2(
-          speed.x * direction.x,
-          speed.y * direction.y);
+            speed.x * direction.x,
+            speed.y * direction.y);
 
-        if (isMovingPlatform) {
-            var dist = (transform.position - Camera.main.transform.position).z;
-            var rightBorder = Camera.main.ViewportToWorldPoint(
-                new Vector3(1, 0, dist)
-               ).x;
-            var leftBorder = Camera.main.ViewportToWorldPoint(
-                new Vector3(0, 0, dist)
-               ).x;
+        var dist = (transform.position - Camera.main.transform.position).z;
+        var rightBorder = Camera.main.ViewportToWorldPoint(
+            new Vector3(1, 0, dist)
+        ).x;
+        var leftBorder = Camera.main.ViewportToWorldPoint(
+            new Vector3(0, 0, dist)
+        ).x;
 
-            if (transform.position.x <= leftBorder)
-                direction = new Vector2(1, -1);
-            else if (transform.position.x >= rightBorder)
-                direction = new Vector2(-1, -1);
-        }
-
-
+        if (transform.position.x <= leftBorder)
+            direction = new Vector2(1, -1);
+        else if (transform.position.x >= rightBorder)
+            direction = new Vector2(-1, -1);
     }
 
     void FixedUpdate()
