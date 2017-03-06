c24c98e665042a07524a0bf3fa27fdcd3111fec2
Sk0ut <fcouto1995@gmail.com>
2017-03-06 22:06:32 +0000

    Win by distance on 2 player

---
+++ Assets/Scripts/WinConditions/DistanceWin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceWin : MonoBehaviour {
+
+	public GameObject player1;
+	public GameObject player2;
+	public float maxDistance = 10f;
+	public string winner = "";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player1.transform.position.y - player2.transform.position.y > maxDistance) {
+			winner = "Player1";	
+		} else if (player2.transform.position.y - player1.transform.position.y > maxDistance) {
+			winner = "Player2";
+		}
+	}
+}
