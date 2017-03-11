2522087dd4433a402e411ec458174a9a60110c2f
Sk0ut <fcouto1995@gmail.com>
2017-03-11 15:24:41 +0000

    Change camera Update to FixedUpdate

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -6,15 +6,10 @@ public class CameraMovement : MonoBehaviour
 {
     public GameObject player;
 
-    // Use this for initialization
-    void Start()
-    {
-    }
-
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        var delta = player.transform.position.y - transform.position.y;
+        float delta = player.transform.position.y - transform.position.y;
         if (delta > 0)
         {
             transform.Translate(new Vector3(0, delta * Time.deltaTime, 0));
