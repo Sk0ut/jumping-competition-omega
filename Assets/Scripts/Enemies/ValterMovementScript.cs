8f507abb7731b76d94f67a2dac25865d53505925
sceptross <up201304205@fe.up.pt>
2017-03-13 19:06:44 +0000

    Small Enemy tweak

--- Assets/Scripts/Enemies/ValterMovementScript.cs
+++ Assets/Scripts/Enemies/ValterMovementScript.cs
@@ -4,42 +4,41 @@ using UnityEngine;
 
 public class ValterMovementScript : MonoBehaviour {
 
-    public Vector2 speed = new Vector2(1, 0);
+    public Vector2 speed = new Vector2(1, 1);
     public Vector2 direction = new Vector2(-1, -1);
 
     public Vector2 movement;
     private Rigidbody2D rigidbodyComponent;
-    public float timeSinceLastSec = 0;
+    public float timeSinceLastSec = 0.9f;
+
+    private float rightBorder;
+    private float leftBorder;
+    private float topBorder;
+    private float bottomBorder;
+
 
     // Use this for initialization
     void Start () {
-    }
-
-    // Update is called once per frame
-    void Update () {
         var dist = (transform.position - Camera.main.transform.position).z;
+
         var rightBorder = Camera.main.ViewportToWorldPoint(
             new Vector3(1, 0, dist)
         ).x;
         var leftBorder = Camera.main.ViewportToWorldPoint(
             new Vector3(0, 0, dist)
         ).x;
+        Debug.Log(leftBorder);
         var topBorder = Camera.main.ViewportToWorldPoint(
            new Vector3(0, 0, dist)
        ).y;
         var bottomBorder = Camera.main.ViewportToWorldPoint(
             new Vector3(0, 1, dist)
         ).y;
+    }
 
+    // Update is called once per frame
+    void Update () {
         timeSinceLastSec += Time.deltaTime;
-        if (transform.position.x <= leftBorder)
-            direction = new Vector2(1, -1);
-        else if (transform.position.x >= rightBorder)
-            direction = new Vector2(-1, -1);
-        else if (transform.position.y <= topBorder)
-            direction = new Vector2(0, -1);
-        else if (transform.position.y >= bottomBorder)
-            direction = new Vector2(0, 1);
         if (timeSinceLastSec < 1) {
             return;
         }
@@ -79,6 +78,15 @@ public class ValterMovementScript : MonoBehaviour {
 
     void FixedUpdate()
     {
+        Debug.Log(transform.position);
+        if (transform.position.x <= leftBorder)
+            direction = new Vector2(1, -1);
+        else if (transform.position.x >= rightBorder)
+            direction = new Vector2(-1, -1);
+        else if (transform.position.y <= topBorder)
+            direction = new Vector2(0, -1);
+        else if (transform.position.y >= bottomBorder)
+            direction = new Vector2(0, 1);
         if (rigidbodyComponent == null) rigidbodyComponent = GetComponent<Rigidbody2D>();
 
         // Apply movement to the rigidbody
