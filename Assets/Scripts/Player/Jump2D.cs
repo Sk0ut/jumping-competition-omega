d691b9f4d3c232331a1c2d307c7a6251894b7cf2
quartz55 <john.razor97@gmail.com>
2017-03-13 19:52:19 +0000

    Platform animations

--- Assets/Scripts/Player/Jump2D.cs
+++ Assets/Scripts/Player/Jump2D.cs
@@ -9,34 +9,23 @@ public class Jump2D : MonoBehaviour {
 	// Object which will check if the player is on the ground
 	public Transform CheckGround;
 	public Rigidbody2D Rb;
-    private bool _onGround;
 
     public event EventManager.EventAction OnPlayerJump;
 
     private void Start()
 	{
 		Rb = GetComponent<Rigidbody2D>();
-		_onGround = false;
 	}
 
     private void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.CompareTag("Plataform_static") || coll.gameObject.CompareTag("Plataform_move")) {
-			_onGround = true;
-		}
-	}
-
-    private void OnCollisionExit2D(Collision2D coll) {
-		if (coll.gameObject.CompareTag("Plataform_static") || coll.gameObject.CompareTag("Plataform_move")) {
-			_onGround = false;
-		}
-	}
-
-    private void FixedUpdate () {
-		if (_onGround && Rb.velocity.y <= 0)
-		{
-		    if (OnPlayerJump != null) OnPlayerJump();
-		    Rb.velocity = new Vector2 (0, 0);
-			Rb.AddForce (new Vector2 (0, JumpHeight));
+		    if (Rb.velocity.y <= 0)
+		    {
+		        if (OnPlayerJump != null) OnPlayerJump();
+		        Rb.velocity = new Vector2 (0, 0);
+		        Rb.AddForce (new Vector2 (0, JumpHeight));
+		        coll.gameObject.transform.FindChild("Platform_Rig").GetComponent<Animator>().SetTrigger("Bouncing");
+		    }
 		}
 	}
 }
