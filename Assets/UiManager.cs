c130bb94df34f262b337a1fd4ffbe3a44fcf93f8
quartz55 <john.razor97@gmail.com>
2017-03-14 10:46:35 +0000

    Main menu + end game buttons

---
+++ Assets/UiManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiManager : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    public void Update()
+    {
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
